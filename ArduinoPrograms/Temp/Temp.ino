6c2e1ed26e98725fa9326087c86da6576fdad8a8
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-15 11:58:03 -0800

    Updated Temp.ino

--- ArduinoPrograms/Temp/Temp.ino
+++ ArduinoPrograms/Temp/Temp.ino
@@ -5,3 +5,19 @@
 
 #include <Arduino.h>
 
+static constexpr byte A_IN_PIN = A0;
+
+void setup(void)
+{
+    Serial.begin(57600);
+    Serial.println();
+    Serial.println("STARTED SERIAL MONITOR AT 57600 BAUD");
+}
+
+void loop(void)
+{
+    int rd = analogRead(A_IN_PIN);
+    Serial.print("READ: ");
+    Serial.println(rd);
+    delay(1000);
+}
