2308ea1425ee7e911b543fcae5f404ae78ef1d56
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-15 10:41:26 -0800

    Added ArduinoPrograms

---
+++ ArduinoPrograms/CPUMonitor/CPUMonitor.ino
@@ -0,0 +1,48 @@
+/**
+ *  AUTHOR:  Maximilian S Puglielli (MSP)
+ *  DATE:    2020.12.13
+**/
+
+#include <Arduino.h>
+
+static constexpr byte ADDR_CNT = 12;
+static constexpr byte ADDR_PINS[ADDR_CNT] = { 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+static constexpr byte CLK = 3;
+
+void setup(void)
+{
+    Serial.begin(57600);
+
+    {
+        const byte* ptr = ADDR_PINS;
+        const byte* const end = ptr + ADDR_CNT;
+        for (; ptr < end; ptr++)
+            pinMode(*ptr, INPUT);
+    }
+    pinMode(CLK, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(CLK), OnClockRise, 2);
+}
+
+void loop(void)
+{}
+
+void OnClockRise(void)
+{
+    unsigned short addr = 0;
+    {
+        const byte* ptr = ADDR_PINS;
+        const byte* const end = ptr + ADDR_CNT;
+        for (; ptr < end; ptr++)
+        {
+            addr <<= 1;
+            addr |= (digitalRead(*ptr) ? 1 : 0);
+        }
+    }
+    addr &= 0x0FFF;
+    Serial.print(addr, BIN);
+    Serial.print("    ");
+    Serial.print(addr, HEX);
+    Serial.print("    ");
+    Serial.println(addr, DEC);
+}
