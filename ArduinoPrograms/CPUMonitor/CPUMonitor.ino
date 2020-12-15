30df699152aa045543b05578fd95818e1f77f33b
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-15 12:33:52 -0800

    Updated CPUMonitor.ino

--- ArduinoPrograms/CPUMonitor/CPUMonitor.ino
+++ ArduinoPrograms/CPUMonitor/CPUMonitor.ino
@@ -1,47 +1,57 @@
 /**
  *  AUTHOR:  Maximilian S Puglielli (MSP)
- *  DATE:    2020.12.13
+ *  CREATED: 2020.12.13
 **/
 
 #include <Arduino.h>
 
-static constexpr byte ADDR_CNT = 12;
-static constexpr byte ADDR_PINS[ADDR_CNT] = { 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-static constexpr byte CLK = 2;
+static constexpr byte CLOCK = 2;
+static constexpr byte ADDR_BUS_DIGITAL_CNT = 12;
+static constexpr byte ADDR_BUS_DIGITAL[ADDR_BUS_DIGITAL_CNT] = { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 1 };
+// static constexpr byte ADDR_BUS_ANALOG_CNT = 4;
+// static constexpr byte ADDR_BUS_ANALOG[ADDR_BUS_ANALOG_CNT] = { A3, A2, A1, A0 };
 
 void setup(void)
 {
     Serial.begin(57600);
     Serial.println();
-    Serial.println("SERIAL STARTED 57600");
+    Serial.println("SERIAL MONITOR STARTED AT 57600 BAUD");
 
+    pinMode(CLOCK, INPUT);
     {
-        const byte* ptr = ADDR_PINS;
-        const byte* const end = ptr + ADDR_CNT;
+        const byte* ptr = ADDR_BUS_DIGITAL;
+        const byte* const end = ptr + ADDR_BUS_DIGITAL_CNT;
         for (; ptr < end; ptr++)
             pinMode(*ptr, INPUT);
     }
-    pinMode(CLK, INPUT);
 
-    attachInterrupt(digitalPinToInterrupt(CLK), OnClockRise, FALLING);
+    attachInterrupt(digitalPinToInterrupt(CLOCK), OnClockRise, FALLING);
 }
 
-void loop(void)
-{}
+void loop(void) {}
 
 void OnClockRise(void)
 {
-    unsigned short addr = 0;
+    // unsigned int addr = ( (analogRead(*ADDR_BUS_ANALOG) >= 512) ? 0x1 : 0x0 );
+    // {
+    //     const byte* ptr = ADDR_BUS_ANALOG + 1;
+    //     const byte* const end = ptr + ADDR_BUS_ANALOG_CNT;
+    //     for (; ptr < end; ptr++)
+    //     {
+    //         addr <<= 1;
+    //         addr |= ( (analogRead(*ADDR_BUS_ANALOG) >= 512) ? 0x1 : 0x0 );
+    //     }
+    // }
+    unsigned int addr = (digitalRead(*ADDR_BUS_DIGITAL) ? 0x1 : 0x0);
     {
-        const byte* ptr = ADDR_PINS;
-        const byte* const end = ptr + ADDR_CNT;
+        const byte* ptr = ADDR_BUS_DIGITAL + 1;
+        const byte* const end = ptr + ADDR_BUS_DIGITAL_CNT;
         for (; ptr < end; ptr++)
         {
             addr <<= 1;
-            addr |= (digitalRead(*ptr) ? 1 : 0);
+            addr |= (digitalRead(*ptr) ? 0x1 : 0x0);
         }
     }
-    addr &= 0x0FFF;
     Serial.print(addr, BIN);
     Serial.print("    ");
     Serial.print(addr, HEX);
