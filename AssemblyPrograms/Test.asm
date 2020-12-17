ddb9bdb615c00af10c270a498c1d23abc8830a87
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-16 22:00:19 -0800

    Updated Test.asm & HelloWorld.asm

--- AssemblyPrograms/Test.asm
+++ AssemblyPrograms/Test.asm
@@ -3,28 +3,58 @@ PORTA = $6001
 DDRB  = $6002
 DDRA  = $6003
 
+E  = %10000000
+RW = %01000000
+RS = %00100000
+
     .org $8000
+
 reset:
-    lda #%11111111
+    lda #%11111111 ; Set all pins on port B to output
     sta DDRB
 
-    lda #%11100000
+    lda #%11100000 ; Set top 3 pins on port A to output
     sta DDRA
 
+    lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set E bit to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #%00001110 ; Display on; cursor on; blink off
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set E bit to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
+    lda #%00000110 ; Increment and shift cursor; don't shift display
+    sta PORTB
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    lda #E         ; Set E bit to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+
 main:
-    lda #'a'
+    lda #"H"
     sta PORTB
-
-    lda #$C0
+    lda #RS         ; Set RS; Clear RW/E bits
+    sta PORTA
+    lda #(RS | E)   ; Set E bit to send instruction
+    sta PORTA
+    lda #RS         ; Clear E bits
     sta PORTA
 
-    lda #'z'
-    sta PORTB
-
-    lda #$60
-    sta PORTA
-
-    jmp main
+exit:
+    jmp exit
 
     .org $FFFC
     .word reset
