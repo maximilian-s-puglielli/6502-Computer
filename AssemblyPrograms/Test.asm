51f607c8f564292df20914879db164469df508bf
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-16 22:07:57 -0800

    Updated Test.asm

--- AssemblyPrograms/Test.asm
+++ AssemblyPrograms/Test.asm
@@ -10,41 +10,50 @@ RS = %00100000
     .org $8000
 
 reset:
-    lda #%11111111 ; Set all pins on port B to output
+    lda #%11111111  ; Set all pins on port B to output
     sta DDRB
 
-    lda #%11100000 ; Set top 3 pins on port A to output
+    lda #%11100000  ; Set top 3 pins on port A to output
     sta DDRA
 
-    lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+    lda #%00111000  ; Set 8-bit mode, 2-line display, 5x8 font
     sta PORTB
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
-    lda #E         ; Set E bit to send instruction
+    lda #E          ; Set E bit to send instruction
     sta PORTA
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
 
-    lda #%00001110 ; Display on; cursor on; blink off
+    lda #%00001110  ; Display on, cursor on, blink off
     sta PORTB
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
-    lda #E         ; Set E bit to send instruction
+    lda #E          ; Set E bit to send instruction
     sta PORTA
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
 
-    lda #%00000110 ; Increment and shift cursor; don't shift display
+    lda #%00000110  ; Increment and shift cursor, don't shift display
     sta PORTB
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
-    lda #E         ; Set E bit to send instruction
+    lda #E          ; Set E bit to send instruction
     sta PORTA
-    lda #0         ; Clear RS/RW/E bits
+    lda #0          ; Clear RS/RW/E bits
     sta PORTA
 
 main:
     lda #"H"
+    sta PORTB
+    lda #RS         ; Set RS; Clear RW/E bits
+    sta PORTA
+    lda #(RS | E)   ; Set E bit to send instruction
+    sta PORTA
+    lda #RS         ; Clear E bits
+    sta PORTA
+
+    lda #"e"
     sta PORTB
     lda #RS         ; Set RS; Clear RW/E bits
     sta PORTA
