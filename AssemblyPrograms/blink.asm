3cce6bf9af612d698f3806aa4bd7732c675b1a39
maximilian-s-puglielli <maximilian.s.puglielli@gmail.com>
2020-12-16 21:41:06 -0800

    Updated Blink.asm

--- AssemblyPrograms/blink.asm
+++ AssemblyPrograms/blink.asm
@@ -1,19 +1,31 @@
+PORTB = $6000
+PORTA = $6001
+DDRB  = $6002
+DDRA  = $6003
+
     .org $8000
+reset:
     lda #$FF
     sta $6002
 
     lda #$E0
-    sta $6001
+    sta $6003
 
 loop:
     lda #$55
     sta $6000
 
+    lda #$C0
+    sta $6001
+
     lda #$AA
     sta $6000
+
+    lda #$60
+    sta $6001
 
     jmp loop
 
     .org $FFFC
-    .word $8000
+    .word reset
     .word $0000
